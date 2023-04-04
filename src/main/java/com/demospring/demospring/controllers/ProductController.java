4c4272a7a6231bbe98ecf71a5c90b32f3eb1a603
Juan Camilo Londoño <londono652@gmail.com>
2023-04-04 15:23:28 -0500

    se agregan metodos, dockerfile y azure pipeline

--- src/main/java/com/demospring/demospring/controllers/ProductController.java
+++ src/main/java/com/demospring/demospring/controllers/ProductController.java
@@ -1,6 +1,7 @@
 package com.demospring.demospring.controllers;
 
 import com.demospring.demospring.models.entity.Product;
+import com.demospring.demospring.repositories.ProductRepository;
 import com.demospring.demospring.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,11 +24,11 @@ public class ProductController {
 
 
     @PostMapping()
-    public ResponseEntity<Product> create(@RequestBody Product product){
+    public ResponseEntity<Product> create(@RequestBody Product product) {
         try {
             var _product = productService.createProduct(product);
             return new ResponseEntity<>(_product, HttpStatus.OK);
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -35,7 +36,7 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Product> list(@PathVariable("id") long id){
+    public ResponseEntity<Product> list(@PathVariable("id") long id) {
         Optional<Product> _product = productService.getProduct(id);
         if (_product.isPresent()) {
             return new ResponseEntity<>(_product.get(), HttpStatus.OK);
@@ -45,10 +46,33 @@ public class ProductController {
 
     }
 
-    @DeleteMapping
-    public void delete(){}
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteproduct(@PathVariable("id") long id) {
+        try {
+            productService.deleteProduct(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable("id") long id, @RequestBody Product product) {
+        var _productupdated = productService.getProduct(id);
+        if (_productupdated.isPresent()) {
+            Product newupdatedproduct = _productupdated.get();
+            newupdatedproduct.setName(product.getName());
+            newupdatedproduct.setAmount(product.getAmount());
+            newupdatedproduct.setDescription(product.getDescription());
+            newupdatedproduct.setType(product.getType());
+            newupdatedproduct.setBarCode(product.getBarCode());
+            newupdatedproduct.setUnitPrice(product.getUnitPrice());
+            return new ResponseEntity<>(productService.updateProduct(newupdatedproduct), HttpStatus.OK);
+        } else {
 
-    @PutMapping
-    public void update(){}
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
 }
+
